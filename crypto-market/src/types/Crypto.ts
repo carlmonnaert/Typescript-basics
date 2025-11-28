dc404c29c5b1a08faadf962f366968a9795c808d
Carl <monnaert.carl@gmail.com>
2025-11-28 01:32:46 +0100

    Add files via upload

---
+++ crypto-market/src/types/Crypto.ts
@@ -0,0 +1,10 @@
+// src/types/Crypto.ts
+export interface Crypto {
+    id: string;
+    name: string;
+    image: string;
+    current_price: number;
+    symbol: string;
+    price_change_percentage_24h: number;
+  }
+  
